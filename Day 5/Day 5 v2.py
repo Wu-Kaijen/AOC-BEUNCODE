16f75ae4618b8ae0ef4286cebfd693fac6077124
Wu Kaijen <75332096+Wu-Kaijen@users.noreply.github.com>
2020-12-05 15:45:03 +0100

    Add files via upload

---
+++ Day 5/Day 5 v2.py
@@ -0,0 +1,21 @@
+import numpy as np
+SEAT = open('SEATS.csv','r').read().replace('B','1').replace('F','0').replace('L','0').replace('R','1').split()
+x = []
+SEATS = np.zeros((128, 8))
+for i in range(len(SEAT)):
+    ROW = int(SEAT[i][0:7],2)
+    Col = int(SEAT[i][7::],2)
+    SEATS[ROW,Col] = 1
+    x.append(ROW*8 + Col)
+print (max(x))
+SEATS[0:2] =1
+SEATS[120:128] =1    
+print(np.where(SEATS == 0))    
+
+
+    
+
+
+    
+                
+            
