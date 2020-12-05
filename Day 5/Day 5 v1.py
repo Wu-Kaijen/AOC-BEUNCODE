6307c0d3e0fcd51a8d038fdd5d71b9ad3192e2d2
Wu Kaijen <75332096+Wu-Kaijen@users.noreply.github.com>
2020-12-05 23:38:35 +0100

    Add files via upload

--- Day 5/Day 5 v1.py
+++ Day 5/Day 5 v1.py
@@ -1,51 +1,27 @@
-import math
 import numpy as np
 
-#%% part 1
 x = []
-with open('SEATS.csv','r') as SEAT:
-    for SEAT in SEAT.readlines():
-        Row = list(range(128))
-        Col = list(range(8))
-        for i in range(len(SEAT)):
-            if i <= 6:
-                if SEAT[i] == 'F' :
-                    Row = Row[0:math.floor(len(Row)//2)]
-                else:
-                    Row = Row[math.ceil(len(Row)//2)::]
+SEATS = np.zeros((128, 8))
+for SEAT in open('SEATS.csv','r').readlines():
+    Row = list(range(128))
+    Col = list(range(8))
+    for i in range(len(SEAT)):
+        if i <= 6:
+            if SEAT[i] == 'F' :
+                Row = Row[0:len(Row)//2]
             else:
-                if SEAT[i] == 'L':
-                    Col = Col[0:math.floor(len(Col)//2)]
-                else:
-                    Col = Col[math.ceil(len(Col)//2)::]
-        x.append(Row[0] *8 +Col[0])
-# print(max(x))
-
-#%%  2
-x = np.zeros((128, 8))
-with open('SEATS.csv','r') as SEAT:
-    for SEAT in SEAT.readlines():
-        Row = list(range(128))
-        Col = list(range(8))
-        for i in range(len(SEAT)):
-            if i <= 6:
-                if SEAT[i] == 'F' :
-                    Row = Row[0:math.floor(len(Row)//2)]
-                else:
-                    Row = Row[math.ceil(len(Row)//2)::]
+                Row = Row[len(Row)//2::]
+        else:
+            if SEAT[i] == 'L':
+                Col = Col[0:len(Col)//2]
             else:
-                if SEAT[i] == 'L':
-                    Col = Col[0:math.floor(len(Col)//2)]
-                else:
-                    Col = Col[math.ceil(len(Col)//2)::]
-        x[Row,Col] = 1
-x[0:2] =1
-x[120:128] =1        
-a,b= np.where(x == 0)
-x = [a,b]
-
-
-
+                Col = Col[len(Col)//2::]
+    x.append(Row[0] *8 +Col[0])
+    SEATS[Row,Col] = 1
+print(max(x))
+SEATS[:2] =1
+SEATS[-6:] =1          
+print(np.where(SEATS == 0))   
     
                 
             
--- Day 5/Day 5 v2.py
+++ Day 5/Day 5 v2.py
@@ -5,9 +5,9 @@ SEATS = np.zeros((128, 8))
 for i in range(len(SEAT)):
     ROW = int(SEAT[i][0:7],2)
     Col = int(SEAT[i][7::],2)
-    SEATS[ROW,Col] = 1
+    SEATS[ROW,Col] = 1 
     x.append(ROW*8 + Col)
-print (max(x))
+print ('max id = ', max(x))
 SEATS[0:2] =1
 SEATS[120:128] =1    
 print(np.where(SEATS == 0))    
